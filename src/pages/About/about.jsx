7e1f1a02a355e28f8b51b64b1d617412634c7fb5
Yasmina1509 <xikmatullaevayasmina115@gmail.com>
2024-09-04 17:14:33 +0500

    about

--- src/pages/About/about.jsx
+++ src/pages/About/about.jsx
@@ -6,11 +6,30 @@ import google from "../About/assets/google.png";
 import microsoft from "../About/assets/microsoft.png";
 import airbnb from "../About/assets/airbnb.png";
 import pinterest from "../About/assets/pinterest.png";
+import DavisRosser from '../About/assets/Ellipse 1.png';
+import TianaGeidt from '../About/assets/Ellipse 1 (1).png';
+
 
 function AboutPage() {
+  const comments = [
+    {
+      id: 0,
+      title: "“One of the advantages of utilizing freelancers is finding talent with different skills quickly as our needs change.”",
+      profile: DavisRosser,
+      name: "Davis Rosser",
+    },
+    {
+      id: 1,
+      title: "“Nguli enables us to differentiate ourselves from our competitors and produce content at a higher caliber.”",
+      profile: TianaGeidt,
+      name: "Tiana Geidt",
+    },
+    // You can add more comments as needed
+  ];
+
   return (
     <div className="App">
-      <section className="about-us">
+      <div className="about-container">
         <div className="about-text">
           <h2>About Us</h2>
           <p>
@@ -20,17 +39,14 @@ function AboutPage() {
             diam, diam elit, et risus. Sed purus, aliquet vel ut ipsum
             consectetur a turpis.
           </p>
-
           <button className="join-button">Join Now</button>
-
         </div>
         <div className="about-images">
           <img src={writer} alt="Image 1" />
-          <img src={man} alt="Image 3" />
           <img src={office} alt="Image 2" />
-          
+          <img src={man} alt="Image 3" />
         </div>
-      </section>
+      </div>
 
       <section className="trusted-by">
         <h3>Trusted by:</h3>
@@ -41,9 +57,12 @@ function AboutPage() {
           <img src={pinterest} alt="Pinterest" />
         </div>
       </section>
-
+      
       <section className="our-numbers">
         <h3>Our numbers:</h3>
+
+        
+
         <div className="stats">
           <div className="stat-card">
             <h4>20,583+</h4>
@@ -65,21 +84,14 @@ function AboutPage() {
       </section>
 
       <section className="testimonials">
-        <h3>Trusted by leading brands and startups</h3>
-        <div className="testimonial-card">
-          <p>
-            “One of the advantages of utilizing freelancers is finding talent
-            with different skills quickly as our needs change.”
-          </p>
-          <span>- Davis Rosser, Director of Content Experience</span>
-        </div>
-        <div className="testimonial-card">
-          <p>
-            “Ngull enables us to differentiate ourselves from our competitors
-            and produce content at a higher caliber.”
-          </p>
-          <span>- Tiana Geidt, Director of Content Experience</span>
-        </div>
+        {comments.map((comment) => (
+          <div key={comment.id} className="testimonial-card">
+            <p>{comment.title}</p>
+            <img src={comment.profile} alt={comment.name} />
+            <h4>{comment.name}</h4>
+            <span>{comment.position}</span>
+          </div>
+        ))}
       </section>
     </div>
   );
